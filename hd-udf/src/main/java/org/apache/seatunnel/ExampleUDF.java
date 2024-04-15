b86c682023c24b52a666a9e613748fd5396e05e4
xiaofu <wfygowxf1@163.com>
2024-04-15 11:06:36 +0800

    hd-dev分支用来做udf开发,主代码不变

---
+++ hd-udf/src/main/java/org/apache/seatunnel/ExampleUDF.java
@@ -0,0 +1,28 @@
+package org.apache.seatunnel;
+
+import com.google.auto.service.AutoService;
+import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.transform.sql.zeta.ZetaUDF;
+
+import java.util.List;
+
+@AutoService(ZetaUDF.class)
+public class ExampleUDF implements ZetaUDF {
+    @Override
+    public String functionName() {
+        return "EXAMPLE";
+    }
+
+    @Override
+    public SeaTunnelDataType<?> resultType(List<SeaTunnelDataType<?>> argsType) {
+        return BasicType.STRING_TYPE;
+    }
+
+    @Override
+    public Object evaluate(List<Object> args) {
+        String arg = (String) args.get(0);
+        if (arg == null) return null;
+        return "UDF: " + arg;
+    }
+}
---
+++ hd-udf/src/test/java/org/apache/seatunnel/ExampleUDFTest.java
@@ -0,0 +1,12 @@
+package org.apache.seatunnel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleUDFTest {
+
+    @Test
+    void evaluate() {
+    }
+}
